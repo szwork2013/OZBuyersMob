7d97cc051ceb8ca95cee247dc0e16aad672f1178
Prajyot <prajyot@giantleapsystems.com>
2014-08-06 14:46:38 +0530

    my order list structure changed

--- app/src/main/java/com/gls/orderzapp/MyOrders/MyOrdersListAdapters/SubOrderProductListAdapter.java
+++ app/src/main/java/com/gls/orderzapp/MyOrders/MyOrdersListAdapters/SubOrderProductListAdapter.java
@@ -1,5 +1,6 @@
 package com.gls.orderzapp.MyOrders.MyOrdersListAdapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
@@ -8,9 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.gls.orderzapp.MyOrders.Beans.ProductConfiguration;
 import com.gls.orderzapp.MyOrders.Beans.ProductDetails;
 import com.gls.orderzapp.R;
 import com.google.gson.Gson;
@@ -33,7 +36,6 @@ public class SubOrderProductListAdapter extends BaseAdapter {
     ImageLoader imageLoader;
     DisplayImageOptions options;
     Typeface tfRobotoNormal;
-    boolean isVisible = false;
 
     public SubOrderProductListAdapter(Context context, List<ProductDetails> productDetailsList) {
         this.context = context;
@@ -65,18 +67,86 @@ public class SubOrderProductListAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
+//        if (convertView == null) {
+//            LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            convertView = li.inflate(R.layout.sub_order_list_content, null);
+//        }
+//        ImageView imageProductLogo = (ImageView) convertView.findViewById(R.id.product_logo);
+//        TextView txt_product_name = (TextView) convertView.findViewById(R.id.txt_product_name);
+//        TextView textProductPrice = (TextView) convertView.findViewById(R.id.product_price);
+//        TextView quantity = (TextView) convertView.findViewById(R.id.quantity);
+//        TextView uom = (TextView) convertView.findViewById(R.id.txt_uom);
+//        TextView actual_price = (TextView) convertView.findViewById(R.id.actual_price);
+//        final ImageView image = (ImageView) convertView.findViewById(R.id.image);
+//
+//        String imageLogo = new Gson().toJson(productDetailsList.get(position));
+//        try {
+//            if (imageLogo != null && imageLogo.contains("productlogo")) {
+//                imageLoader = com.nostra13.universalimageloader.core.ImageLoader.getInstance();
+//                imageLoader.init(ImageLoaderConfiguration.createDefault(context));
+//                imageLoader.displayImage(productDetailsList.get(position).getProductlogo(), imageProductLogo, options, new SimpleImageLoadingListener() {
+//                    boolean cacheFound;
+//
+//                    @Override
+//                    public void onLoadingStarted(String imageUri, View view) {
+//                        List<String> memCache = MemoryCacheUtil.findCacheKeysForImageUri(imageUri, ImageLoader.getInstance().getMemoryCache());
+//                        cacheFound = !memCache.isEmpty();
+//                        if (!cacheFound) {
+//                            File discCache = DiscCacheUtil.findInCache(imageUri, ImageLoader.getInstance().getDiscCache());
+//                            if (discCache != null) {
+//                                cacheFound = discCache.exists();
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+//                        if (cacheFound) {
+////                        MemoryCacheUtil.removeFromCache(imageUri, ImageLoader.getInstance().getMemoryCache());
+////                        DiscCacheUtil.removeFromCache(imageUri, ImageLoader.getInstance().getDiscCache());
+//
+//                            ImageLoader.getInstance().displayImage(imageUri, (ImageView) view);
+//                        }
+//                    }
+//                });
+//            }
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//        convertView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(context, "clicked", Toast.LENGTH_LONG).show();
+//                if(isVisible == false){
+//                    isVisible = true;
+//                    image.setVisibility(View.VISIBLE);
+//                }else{
+//                    isVisible = false;
+//                    image.setVisibility(View.GONE);
+//                }
+//            }
+//        });
+//        textProductPrice.setTypeface(tfRobotoNormal);
+//        quantity.setTypeface(tfRobotoNormal);
+//        actual_price.setTypeface(tfRobotoNormal);
+//        txt_product_name.setTypeface(tfRobotoNormal);
+//        textProductPrice.setText(String.format("%.2f", productDetailsList.get(position).getBaseprice()));
+//        quantity.setText(String.format("%.2f", productDetailsList.get(position).getQty()));
+//        uom.setText(productDetailsList.get(position).getUom());
+//        txt_product_name.setText(productDetailsList.get(position).getProductname());
+//        actual_price.setText(String.format("%.2f", productDetailsList.get(position).getOrderprice()));
+
         if (convertView == null) {
             LayoutInflater li = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = li.inflate(R.layout.sub_order_list_content, null);
+            convertView = li.inflate(R.layout.my_suborder_list_content, null);
         }
+
         ImageView imageProductLogo = (ImageView) convertView.findViewById(R.id.product_logo);
-        TextView txt_product_name = (TextView) convertView.findViewById(R.id.txt_product_name);
-        TextView textProductPrice = (TextView) convertView.findViewById(R.id.product_price);
-        TextView quantity = (TextView) convertView.findViewById(R.id.quantity);
-        TextView uom = (TextView) convertView.findViewById(R.id.txt_uom);
-        TextView actual_price = (TextView) convertView.findViewById(R.id.actual_price);
-        final ImageView image = (ImageView) convertView.findViewById(R.id.image);
+        TextView txt_product_name = (TextView) convertView.findViewById(R.id.product_name);
+        TextView order_price = (TextView) convertView.findViewById(R.id.order_price);
+        ImageView product_price_details_image = (ImageView) convertView.findViewById(R.id.product_price_details_image);
 
         String imageLogo = new Gson().toJson(productDetailsList.get(position));
         try {
@@ -109,32 +179,83 @@ public class SubOrderProductListAdapter extends BaseAdapter {
                     }
                 });
             }
+
+            if(productDetailsList.get(position).getProductname() != null){
+                txt_product_name.setText(productDetailsList.get(position).getProductname());
+            }
+
+            order_price.setText(String.format("%.2f", productDetailsList.get(position).getOrderprice()));
+
+            convertView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    displayPriceDetailsInDialog(productDetailsList.get(position));
+                }
+            });
         }catch (Exception e){
             e.printStackTrace();
         }
-
-        convertView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(context, "clicked", Toast.LENGTH_LONG).show();
-                if(isVisible == false){
-                    isVisible = true;
-                    image.setVisibility(View.VISIBLE);
-                }else{
-                    isVisible = false;
-                    image.setVisibility(View.GONE);
-                }
-            }
-        });
-        textProductPrice.setTypeface(tfRobotoNormal);
-        quantity.setTypeface(tfRobotoNormal);
-        actual_price.setTypeface(tfRobotoNormal);
-        txt_product_name.setTypeface(tfRobotoNormal);
-        textProductPrice.setText(String.format("%.2f", productDetailsList.get(position).getBaseprice()));
-        quantity.setText(String.format("%.2f", productDetailsList.get(position).getQty()));
-        uom.setText(productDetailsList.get(position).getUom());
-        txt_product_name.setText(productDetailsList.get(position).getProductname());
-        actual_price.setText(String.format("%.2f", productDetailsList.get(position).getOrderprice()));
         return convertView;
+    }
+
+    void displayPriceDetailsInDialog(ProductDetails productDetails){
+        try {
+            LayoutInflater li = LayoutInflater.from(context);
+            View dialogView = li.inflate(R.layout.my_order_price_details_dialog, null);
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+            // set prompts.xml to alertdialog builder
+            alertDialogBuilder.setView(dialogView);
+
+            TextView product_base_price = (TextView) dialogView.findViewById(R.id.product_base_price);
+            TextView product_quantity = (TextView) dialogView.findViewById(R.id.product_quantity);
+            TextView product_configuration_price = (TextView) dialogView.findViewById(R.id.product_configuration_price);
+            TextView total_product_price = (TextView) dialogView.findViewById(R.id.total_product_price);
+            LinearLayout llCloseDialogImage = (LinearLayout) dialogView.findViewById(R.id.llCloseDialogImage);
+
+
+            product_base_price.setText(productDetails.getBaseprice() + "");
+
+
+            product_quantity.setText(productDetails.getQty()+" "+productDetails.getUom());
+
+            product_configuration_price.setText(configurationPrice(productDetails.getProductconfiguration())+"");
+
+            total_product_price.setText(productDetails.getOrderprice()+"");
+            // create alert dialog
+            final AlertDialog alertDialog = alertDialogBuilder.create();
+
+            alertDialog.setInverseBackgroundForced(true);
+
+            // show it
+            alertDialog.show();
+
+
+            llCloseDialogImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    alertDialog.cancel();
+                }
+            });
+
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
+    }
+
+    public double configurationPrice(List<ProductConfiguration> list) {
+        double configurationPrice = 0.0;
+        try {
+            for (int i = 0; i < list.size(); i++) {
+                configurationPrice = configurationPrice + list.get(i).getProd_configprice().getValue();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return configurationPrice;
     }
 }
