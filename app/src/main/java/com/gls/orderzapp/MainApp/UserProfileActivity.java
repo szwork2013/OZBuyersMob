fa52b100630b1156807f8365a4589eb5c7b9b239
Prajyot <prajyot@giantleapsystems.com>
2014-08-25 15:00:50 +0530

    pull changes

--- app/src/main/java/com/gls/orderzapp/MainApp/UserProfileActivity.java
+++ app/src/main/java/com/gls/orderzapp/MainApp/UserProfileActivity.java
@@ -24,78 +24,78 @@ import org.json.JSONObject;
  * Created by avi on 8/25/14.
  */
 public class UserProfileActivity extends Activity {
-    TextView TextFirstName,TextUserName,TextUserMobNo,TextUserEmailId,TextAddress,TextLangauge;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.userprofileactivity);
-        findViewsById();
-    }
-    public void findViewsById() {
-
-    }
-    private class CheckSessionAsync extends AsyncTask<String, Integer, String> {
-        String connectedOrNot, msg, code, resultOfCheckSession;
-        JSONObject jObj;
-        ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            progressDialog = ProgressDialog.show(SettingsActivity.this, "", "");
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                if (new CheckConnection(getApplicationContext()).isConnectingToInternet()) {
-                    connectedOrNot = "success";
-                    resultOfCheckSession = getSessionStatus();
-                    if (!resultOfCheckSession.isEmpty()) {
-                        jObj = new JSONObject(resultOfCheckSession);
-                        if (jObj.has("success")) {
-                            JSONObject jObjSuccess = jObj.getJSONObject("success");
-                            msg = jObjSuccess.getString("message");
-                        } else {
-                            JSONObject jObjError = jObj.getJSONObject("error");
-                            msg = jObjError.getString("message");
-                            code = jObjError.getString("code");
-                        }
-                    }
-                } else {
-                    connectedOrNot = "error";
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return connectedOrNot;
-        }
-
-        @Override
-        protected void onPostExecute(String connectedOrNot) {
-            try {
-                progressDialog.dismiss();
-                if (connectedOrNot.equals("success")) {
-                    if (!resultOfCheckSession.isEmpty()) {
-
-                        if (jObj.has("success")) {
-                            successResponseOfUser = new Gson().fromJson(loadPreferencesUser(), SuccessResponseOfUser.class);
-                            displayUserData();
-                        } else {
-                            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-                            if (code.equals("AL001")) {
-                                Intent goToSignin = new Intent(SettingsActivity.this, SignInActivity.class);
-                                startActivity(goToSignin);
-                            }
-                        }
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Server is not responding please try again later", Toast.LENGTH_LONG).show();
-                    }
-                } else {
-                    Toast.makeText(getApplicationContext(), "Please check your internet connection", Toast.LENGTH_LONG).show();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    TextView TextFirstName,TextUserName,TextUserMobNo,TextUserEmailId,TextAddress,TextLangauge;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.userprofileactivity);
+//        findViewsById();
+//    }
+//    public void findViewsById() {
+//
+//    }
+//    private class CheckSessionAsync extends AsyncTask<String, Integer, String> {
+//        String connectedOrNot, msg, code, resultOfCheckSession;
+//        JSONObject jObj;
+//        ProgressDialog progressDialog;
+//
+//        @Override
+//        protected void onPreExecute() {
+//            progressDialog = ProgressDialog.show(SettingsActivity.this, "", "");
+//        }
+//
+//        @Override
+//        protected String doInBackground(String... params) {
+//            try {
+//                if (new CheckConnection(getApplicationContext()).isConnectingToInternet()) {
+//                    connectedOrNot = "success";
+//                    resultOfCheckSession = getSessionStatus();
+//                    if (!resultOfCheckSession.isEmpty()) {
+//                        jObj = new JSONObject(resultOfCheckSession);
+//                        if (jObj.has("success")) {
+//                            JSONObject jObjSuccess = jObj.getJSONObject("success");
+//                            msg = jObjSuccess.getString("message");
+//                        } else {
+//                            JSONObject jObjError = jObj.getJSONObject("error");
+//                            msg = jObjError.getString("message");
+//                            code = jObjError.getString("code");
+//                        }
+//                    }
+//                } else {
+//                    connectedOrNot = "error";
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            return connectedOrNot;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(String connectedOrNot) {
+//            try {
+//                progressDialog.dismiss();
+//                if (connectedOrNot.equals("success")) {
+//                    if (!resultOfCheckSession.isEmpty()) {
+//
+//                        if (jObj.has("success")) {
+//                            successResponseOfUser = new Gson().fromJson(loadPreferencesUser(), SuccessResponseOfUser.class);
+//                            displayUserData();
+//                        } else {
+//                            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+//                            if (code.equals("AL001")) {
+//                                Intent goToSignin = new Intent(SettingsActivity.this, SignInActivity.class);
+//                                startActivity(goToSignin);
+//                            }
+//                        }
+//                    } else {
+//                        Toast.makeText(getApplicationContext(), "Server is not responding please try again later", Toast.LENGTH_LONG).show();
+//                    }
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Please check your internet connection", Toast.LENGTH_LONG).show();
+//                }
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
