e36c89b2257f4532c25e34cc6dcf6f253854a964
Amit <amit@prodonus-neha-003.(none)>
2014-09-15 16:32:05 +0530

    changes in city done

--- app/src/main/java/com/gls/orderzapp/MainApp/StartUpActivity.java
+++ app/src/main/java/com/gls/orderzapp/MainApp/StartUpActivity.java
@@ -432,6 +432,7 @@ public class StartUpActivity extends Activity implements View.OnClickListener {
         }
         else{
             selectedCityName.setText(R.string.selected_city);
+
             cityName.setVisibility(View.VISIBLE);
         }
 
@@ -567,6 +568,7 @@ public class StartUpActivity extends Activity implements View.OnClickListener {
             if(loadSearchByCityPreference().isEmpty()||loadSearchByCityPreference()==null||loadSearchByCityPreference().equalsIgnoreCase("All")){
                 resultGetProviderAndProduct = ServerConnection.executeGet(getApplicationContext(), "/api/searchproduct/" + param);
             }else{
+
                 resultGetProviderAndProduct = ServerConnection.executeGet(getApplicationContext(), "/api/searchproduct/" + param+"?city="+loadSearchByCityPreference());
             }
 
@@ -696,7 +698,9 @@ public class StartUpActivity extends Activity implements View.OnClickListener {
         String city,city1 = "";
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         city = sp.getString("SEARCH_CITY","");
-        city1 = city.substring(0,1).toUpperCase() + city.substring(1);
+        if(city.length()>0) {
+            city1 = city.substring(0, 1).toUpperCase() + city.substring(1);
+        }
 
 
 
